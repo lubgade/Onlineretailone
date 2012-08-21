e64b586acd7d06dbd05a5093b9ccd39cb40aebcc
Leena Ghanekar <lubgade@yahoo.com>
2012-08-21 17:08:18 +0000

    git-svn-id: svn://10.0.1.61/Onlineretailone@408 920c45cc-f926-1845-ac39-3e4767a7a8b5

--- src/org/apache/gaelucene/auth/GAELuceneUserJDO.java
+++ src/org/apache/gaelucene/auth/GAELuceneUserJDO.java
@@ -23,7 +23,7 @@ import java.util.List;
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 
-import com.cloudjini.onlineretail.Dao.PMF;
+import com.adjointweb.onlineretail.Dao.PMF;
 
 /**
  * User Entity JDO
